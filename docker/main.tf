386856a99238ca6ce24b9207d87e906655a7d3cd
chiwenchen <cwchen2000@gmail.com>
2021-06-28 18:07:26 +0800

    add docker tf

---
+++ docker/main.tf
@@ -0,0 +1,26 @@
+locals {
+  port = 4000
+}
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = local.port
+  }
+}
